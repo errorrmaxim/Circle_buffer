79b2c00674f1c474134ad32c00513a1489acdc74
errorrmaxim <errorrmaxim@gmail.com>
2017-03-10 12:56:34 +0200

    second edition (with main.cpp release) / tests have done

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,15 @@
 
 int main(int argc, char *argv[])
 {
+    ring_buff<int, 10>crc_buf;
 
+    for(int i = 0; i < 5; i++)
+    {
+        crc_buf.push(i);
+    }
+    crc_buf.show_buffer();
+
+    //tests heve done!!!
 
 
     return 0;
